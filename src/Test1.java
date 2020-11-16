1d36cdb70e805220c0a9b7f5e52c7a7bdceaabd6
ainarejos <ainarejos2@nigul.cide.es>
2020-11-16 19:57:08 +0100

    Se ha añadido un segundo trabajador.

--- src/Test1.java
+++ src/Test1.java
@@ -1,3 +1,4 @@
+
 class trabajador{
     private String nombre;
     private String apellido;
@@ -10,15 +11,20 @@ class trabajador{
         nombre=e;
         apellido=b;
         puesto=c;
-
     }
 }
 
 
 class test1{
     public static void main(String[] args) {
+        System.out.println("1r trabajador");
         trabajador tofol=new trabajador();
-        tofol.setall("Manolo","Cuesta","informatico");
+        tofol.setall("Tofol","Cuesta","informatico");
         tofol.datos();
+
+        System.out.println("2n trabajador");
+        trabajador Manolo=new trabajador();
+        Manolo.setall("Manolo","Cuesta","informatico");
+        Manolo.datos();
     }
 }
